04feb4833979196a3205fa7cff47784ffcc21a46
S M Afnan Haider <64026942+Afn377@users.noreply.github.com>
2025-03-24 04:13:41 -0400

    finished coffee shop

---
+++ Coffee Shop/app/index.jsx
@@ -0,0 +1,86 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+import icedCoffeeImg from "@/assets/images/iced-coffee.png";
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={icedCoffeeImg}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>Coffee Shop</Text>
+        <Link href="/menu" style={{ marginHorizontal: "auto" }} asChild>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Our Menu</Text>
+          </Pressable>
+        </Link>
+        <Link
+          href="/contact"
+          style={{ marginHorizontal: "auto", marginTop: 10 }}
+          asChild
+        >
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Contact Us</Text>
+          </Pressable>
+        </Link>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  text: {
+    marginBottom: 120,
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  link: {
+    color: "white",
+    fontSize: 42,
+    textDecorationLine: "underline",
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 4,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  button: {
+    height: 60,
+    borderRadius: 20,
+    backgroundColor: "rgba(0, 0, 0, 0.75)",
+    justifyContent: "center",
+
+    padding: 6,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+});
+
+export default app;
